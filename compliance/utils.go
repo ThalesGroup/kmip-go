b5b25127baa699d2929ef6975fc78a3eb3bce329
Russ Egan <russellegan@gmail.com>
2018-09-19 13:33:44 -0500

    support for comparing kmip test case samples
    parses the xml formatted kmip test case samples into a data structure, which can be compared with actual result.  Supports the use of variables for values which vary by test case.

--- compliance/utils.go
+++ compliance/utils.go
@@ -2,16 +2,17 @@ package compliance
 
 import (
 	"encoding/xml"
+	"github.com/pmezard/go-difflib/difflib"
 	"regexp"
+	"strings"
 )
 
 type TTLV struct {
-	XMLName   xml.Name
-	Tag       string  `xml:"tag,omitempty,attr"`
-	Type      string  `xml:"type,attr,omitempty"`
-	Value     string  `xml:"value,attr,omitempty"`
-	Children  []*TTLV `xml:",any"`
-	variables map[string]string
+	XMLName  xml.Name
+	Tag      string  `xml:"tag,omitempty,attr"`
+	Type     string  `xml:"type,attr,omitempty"`
+	Value    string  `xml:"value,attr,omitempty"`
+	Children []*TTLV `xml:",any"`
 }
 
 func (v *TTLV) GetTag() string {
@@ -21,42 +22,52 @@ func (v *TTLV) GetTag() string {
 	return v.Tag
 }
 
-func (v *TTLV) Cmp(v2 *TTLV) bool {
+func Compare(v1, v2 *TTLV) (eq bool, vars map[string]string, diff string) {
+	vars = map[string]string{}
+	eq = compare(v1, v2, vars)
+	if !eq {
+		diff = diffS(v1, v2, vars)
+	}
+	return
+}
+
+func compare(v1, v2 *TTLV, vars map[string]string) bool {
 	switch {
-	case v == nil && v2 == nil:
+	case v1 == nil && v2 == nil:
 		return true
-	case v == nil && v2 != nil:
+	case v1 == nil && v2 != nil:
 		return false
-	case v != nil && v2 == nil:
+	case v1 != nil && v2 == nil:
 		return false
-	case v.Tag != v2.Tag:
+	case v1.GetTag() != v2.GetTag():
 		return false
-	case v.Type != v2.Type:
+	case v1.Type != v2.Type:
 		return false
-	case v.Value != v2.Value:
-		if !isVariable(v.Value) {
+	case v1.Value != v2.Value:
+		if !isVariable(v1.Value) {
 			return false
 		}
-		if v.variables == nil {
-			v.variables = map[string]string{}
-		}
-		if varV, ok := v.variables[v.Value]; ok {
+		if varV, ok := vars[v1.Value]; ok {
 			if varV != v2.Value {
 				return false
 			}
 		} else {
-			v.variables[v.Value] = v2.Value
+			vars[v1.Value] = v2.Value
 		}
 	}
-	if len(v.Children) != len(v2.Children) {
+	if len(v1.Children) != len(v2.Children) {
 		// TODO: this probably isn't quite right, since some attributes
 		// are optional.  Not quite sure how to handle attributes yet
 		return false
 	}
 
+	if len(v1.Children) == 0 {
+		return true
+	}
+
 	// compare non-attribute children.  must be in same order
 	var attrs, attrs2 []*TTLV
-	for i, child := range v.Children {
+	for i, child := range v1.Children {
 		child2 := v2.Children[i]
 		if child.GetTag() == "Attribute" {
 			if child2.GetTag() != "Attribute" {
@@ -65,7 +76,7 @@ func (v *TTLV) Cmp(v2 *TTLV) bool {
 			attrs = append(attrs, child)
 			attrs2 = append(attrs2, child2)
 		} else {
-			if !child.Cmp(child2) {
+			if !compare(child, child2, vars) {
 				return false
 			}
 		}
@@ -104,24 +115,48 @@ func (v *TTLV) Cmp(v2 *TTLV) bool {
 		v1 := attrMap[key]
 		v2 := attrMap2[key]
 		if v1 != v2 {
-			if !isVariable(v.Value) {
+			if !isVariable(v1) {
 				return false
 			}
-			if v.variables == nil {
-				v.variables = map[string]string{}
-			}
-			if varV, ok := v.variables[v.Value]; ok {
+			if varV, ok := vars[v1]; ok {
 				if varV != v2 {
 					return false
 				}
 			} else {
-				v.variables[v.Value] = v2
+				vars[v1] = v2
 			}
 
 		}
 	}
 
 	return true
+}
+
+func diffS(v1, v2 *TTLV, vars map[string]string) string {
+	xml1, err := xml.MarshalIndent(v1, "", "  ")
+	if err != nil {
+		panic(err)
+	}
+	s1 := string(xml1)
+	xml2, err := xml.MarshalIndent(v2, "", "  ")
+	if err != nil {
+		panic(err)
+	}
+	s2 := string(xml2)
+	for k, v := range vars {
+		s1 = strings.Replace(s1, k, v, -1)
+	}
+	diff, _ := difflib.GetUnifiedDiffString(difflib.UnifiedDiff{
+		A:        difflib.SplitLines(s1),
+		B:        difflib.SplitLines(s2),
+		FromFile: "v1",
+		FromDate: "",
+		ToFile:   "v2",
+		ToDate:   "",
+		Context:  1,
+	})
+	return diff
+
 }
 
 func isVariable(s string) bool {
--- compliance/utils_test.go
+++ compliance/utils_test.go
@@ -2,6 +2,7 @@ package compliance
 
 import (
 	"encoding/xml"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"testing"
 )
@@ -25,13 +26,262 @@ func TestTTLV_MarshalXML(t *testing.T) {
 	require.Equal(t, string(b), `<TTLV tag="blue" type="white" value="green"><brown tag="orange" type="black" value="white"></brown></TTLV>`)
 }
 
-//func TestTTLV_Cmp(t *testing.T) {
-//	tests := []struct{
-//		name string
-//		isEq bool
-//		v1 *TTLV
-//		v2 *TTLV
-//	}{
-//
-//	}
-//}
+func TestTTLV_Cmp(t *testing.T) {
+	tests := []struct {
+		name    string
+		isEq    bool
+		v1      *TTLV
+		v2      *TTLV
+		expVars map[string]string
+	}{
+		{
+			name: "empty",
+			v1:   &TTLV{},
+			v2:   &TTLV{},
+			isEq: true,
+		},
+		{
+			name: "basic",
+			v1: &TTLV{
+				XMLName: xml.Name{Local: "blue"},
+				Tag:     "white",
+				Type:    "green",
+				Value:   "red",
+			},
+			v2: &TTLV{
+				XMLName: xml.Name{Local: "blue"},
+				Tag:     "white",
+				Type:    "green",
+				Value:   "red",
+			},
+			isEq: true,
+		},
+		{
+			name: "xmlnameneq",
+			v1: &TTLV{
+				XMLName: xml.Name{Local: "black"},
+				Tag:     "white",
+				Type:    "green",
+				Value:   "red",
+			},
+			v2: &TTLV{
+				XMLName: xml.Name{Local: "blue"},
+				Tag:     "white",
+				Type:    "green",
+				Value:   "red",
+			},
+			isEq: false,
+		},
+		{
+			name: "xmlnameoverridestag",
+			v1: &TTLV{
+				XMLName: xml.Name{Local: "blue"},
+				Tag:     "black",
+				Type:    "green",
+				Value:   "red",
+			},
+			v2: &TTLV{
+				XMLName: xml.Name{Local: "blue"},
+				Tag:     "white",
+				Type:    "green",
+				Value:   "red",
+			},
+			isEq: true,
+		},
+		{
+			name: "typeneq",
+			v1: &TTLV{
+				Tag:   "white",
+				Type:  "blue",
+				Value: "red",
+			},
+			v2: &TTLV{
+				Tag:   "white",
+				Type:  "green",
+				Value: "red",
+			},
+			isEq: false,
+		},
+		{
+			name: "valueneq",
+			v1: &TTLV{
+				Tag:   "white",
+				Type:  "green",
+				Value: "blue",
+			},
+			v2: &TTLV{
+				Tag:   "white",
+				Type:  "green",
+				Value: "red",
+			},
+			isEq: false,
+		},
+		{
+			name: "structure",
+			v1: &TTLV{
+				Tag:  "white",
+				Type: "green",
+				Children: []*TTLV{
+					{Value: "red"},
+					{Value: "green"},
+					{Value: "blue"},
+				},
+			},
+			v2: &TTLV{
+				Tag:  "white",
+				Type: "green",
+				Children: []*TTLV{
+					{Value: "red"},
+					{Value: "green"},
+					{Value: "blue"},
+				},
+			},
+			isEq: true,
+		},
+		{
+			name: "structureneq",
+			v1: &TTLV{
+				Tag:  "white",
+				Type: "green",
+				Children: []*TTLV{
+					{Value: "red"},
+					{Value: "green"},
+					{Value: "blue"},
+				},
+			},
+			v2: &TTLV{
+				Tag:  "white",
+				Type: "green",
+				Children: []*TTLV{
+					{Value: "red"},
+					{Value: "orange"},
+					{Value: "blue"},
+				},
+			},
+			isEq: false,
+		},
+		{
+			name: "structurelenneq",
+			v1: &TTLV{
+				Tag:  "white",
+				Type: "green",
+				Children: []*TTLV{
+					{Value: "red"},
+					{Value: "green"},
+					{Value: "blue"},
+				},
+			},
+			v2: &TTLV{
+				Tag:  "white",
+				Type: "green",
+				Children: []*TTLV{
+					{Value: "red"},
+					{Value: "green"},
+					{Value: "blue"},
+					{Value: "yellow"},
+				},
+			},
+			isEq: false,
+		},
+		{
+			name: "structureorderneq",
+			v1: &TTLV{
+				Tag:  "white",
+				Type: "green",
+				Children: []*TTLV{
+					{Value: "red"},
+					{Value: "green"},
+					{Value: "blue"},
+				},
+			},
+			v2: &TTLV{
+				Tag:  "white",
+				Type: "green",
+				Children: []*TTLV{
+					{Value: "red"},
+					{Value: "blue"},
+					{Value: "green"},
+				},
+			},
+			isEq: false,
+		},
+		{
+			name: "variable",
+			v1: &TTLV{
+				Tag:   "white",
+				Type:  "green",
+				Value: "$COLOR",
+			},
+			v2: &TTLV{
+				Tag:   "white",
+				Type:  "green",
+				Value: "red",
+			},
+			isEq:    true,
+			expVars: map[string]string{"$COLOR": "red"},
+		},
+		{
+			name: "variablebackref",
+			v1: &TTLV{
+				Tag:  "white",
+				Type: "green",
+				Children: []*TTLV{
+					{Value: "$COLOR"},
+					{Value: "blue"},
+					{Value: "$COLOR"},
+				},
+			},
+			v2: &TTLV{
+				Tag:  "white",
+				Type: "green",
+				Children: []*TTLV{
+					{Value: "red"},
+					{Value: "blue"},
+					{Value: "red"},
+				},
+			},
+			isEq:    true,
+			expVars: map[string]string{"$COLOR": "red"},
+		},
+		{
+			name: "variablebackrefneq",
+			v1: &TTLV{
+				Tag:  "white",
+				Type: "green",
+				Children: []*TTLV{
+					{Value: "$COLOR_0"},
+					{Value: "blue"},
+					{Value: "$COLOR_0"},
+				},
+			},
+			v2: &TTLV{
+				Tag:  "white",
+				Type: "green",
+				Children: []*TTLV{
+					{Value: "red"},
+					{Value: "blue"},
+					{Value: "green"},
+				},
+			},
+			isEq:    false,
+			expVars: map[string]string{"$COLOR_0": "red"},
+		},
+	}
+
+	for _, testcase := range tests {
+		t.Run(testcase.name, func(t *testing.T) {
+			eq, vars, diff := Compare(testcase.v1, testcase.v2)
+			if testcase.isEq {
+				assert.True(t, eq)
+			} else {
+				assert.False(t, eq)
+			}
+			if testcase.expVars != nil {
+				assert.Equal(t, testcase.expVars, vars)
+			}
+			if diff != "" {
+				t.Log(diff)
+			}
+		})
+	}
+}
